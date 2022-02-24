aa255feb31854c4f40ea3b32f1b85315e00f2abf
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-02-24 12:56:10 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "intersight" {
-  apikey    = "61d542847564612d3345b882/61d545367564612d31edab46/61ecd1a57564612d315cce24"
+  apikey    = "61d542847564612d3345b882/61d545367564612d31edab46/621701bb7564612d31cb16d9"
   secretkey = "./publickey.pem"
   endpoint  = "https://intersight.com"
 }
